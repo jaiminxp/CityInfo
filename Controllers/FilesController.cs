a43ce8f49d741baa0c7ae232172ccbc7b3bb7066
Jaimin Parmar <jaimins100@gmail.com>
2024-04-04 18:37:57 +0530

    format files

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -4,49 +4,53 @@ using Microsoft.AspNetCore.StaticFiles;
 
 namespace CityInfo.API.Controllers;
 
-    [Route("api/files")]
-    [ApiController]
-    public class FilesController : ControllerBase
+[Route("api/files")]
+[ApiController]
+public class FilesController : ControllerBase
+{
+    private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+    public FilesController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
     {
-        private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
-
-        public FilesController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider) {
-            _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider 
-                ?? throw new System.ArgumentNullException(nameof(fileExtensionContentTypeProvider));
-        }
-
-        [HttpGet("{fileId}")]
-        public ActionResult GetFile(string fileId) {
-            var pathTofile = "csharp-icon.svg";
-
-            if (!System.IO.File.Exists(pathTofile)) {
-                return NotFound();
-            }
-
-            if (!_fileExtensionContentTypeProvider.TryGetContentType(pathTofile, out var contentType)) { 
-                contentType = "application/octet-stream";
-            }
-
-            var bytes = System.IO.File.ReadAllBytes(pathTofile);
-            return File(bytes, contentType, Path.GetFileName(pathTofile));
-        }
-
-        [HttpPost]
-        public async Task<ActionResult> CreateFile(IFormFile file)
-        {
-            if (file.Length == 0 || file.Length > 20971520 || file.ContentType != "application/pdf")
-            {
-                return BadRequest("No file or an invalid one has been inputted.");
-            }
-
-            var path = Path.Combine(Directory.GetCurrentDirectory(), $"uploaded_file_{Guid.NewGuid()}.pdf");
-
-            using (var stream = new FileStream(path, FileMode.Create))
-            {
-                await file.CopyToAsync(stream);
-            }
-
-            return Ok("Your file has been successfully uploaded.");
-        }
+        _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider
+            ?? throw new System.ArgumentNullException(nameof(fileExtensionContentTypeProvider));
     }
+
+    [HttpGet("{fileId}")]
+    public ActionResult GetFile(string fileId)
+    {
+        var pathTofile = "csharp-icon.svg";
+
+        if (!System.IO.File.Exists(pathTofile))
+        {
+            return NotFound();
+        }
+
+        if (!_fileExtensionContentTypeProvider.TryGetContentType(pathTofile, out var contentType))
+        {
+            contentType = "application/octet-stream";
+        }
+
+        var bytes = System.IO.File.ReadAllBytes(pathTofile);
+        return File(bytes, contentType, Path.GetFileName(pathTofile));
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateFile(IFormFile file)
+    {
+        if (file.Length == 0 || file.Length > 20971520 || file.ContentType != "application/pdf")
+        {
+            return BadRequest("No file or an invalid one has been inputted.");
+        }
+
+        var path = Path.Combine(Directory.GetCurrentDirectory(), $"uploaded_file_{Guid.NewGuid()}.pdf");
+
+        using (var stream = new FileStream(path, FileMode.Create))
+        {
+            await file.CopyToAsync(stream);
+        }
+
+        return Ok("Your file has been successfully uploaded.");
+    }
+}
 
