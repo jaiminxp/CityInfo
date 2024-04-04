670640a3fa891484f37ab86b46971abcfa282144
Jaimin Parmar <jaimins100@gmail.com>
2024-04-04 17:08:04 +0530

    file upload

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -30,5 +30,23 @@ namespace MyApp.Namespace
             var bytes = System.IO.File.ReadAllBytes(pathTofile);
             return File(bytes, contentType, Path.GetFileName(pathTofile));
         }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateFile(IFormFile file)
+        {
+            if (file.Length == 0 || file.Length > 20971520 || file.ContentType != "application/pdf")
+            {
+                return BadRequest("No file or an invalid one has been inputted.");
+            }
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), $"uploaded_file_{Guid.NewGuid()}.pdf");
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return Ok("Your file has been successfully uploaded.");
+        }
     }
 }
