45083c1329a67620d1f13ee6c5c661bcd56cde2d
Jaimin Parmar <jaimins100@gmail.com>
2024-04-05 16:32:33 +0530

    add migrations

--- Entities/PointOfInterest.cs
+++ Entities/PointOfInterest.cs
@@ -15,6 +15,9 @@ public class PointOfInterest
     [MaxLength(50)]
     public string Name { get; set; } = string.Empty;
 
+    [MaxLength(200)]
+    public string? Description { get; set; }
+
     [ForeignKey("CityId")]
     public City? City { get; set; }
     public int CityId { get; set; }
