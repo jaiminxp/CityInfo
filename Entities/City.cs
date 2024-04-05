d38f831b0066d7e21ed3600f62829f913e03d9a5
Jaimin Parmar <jaimins100@gmail.com>
2024-04-05 16:20:30 +0530

    remove dto reference from city entity

--- Entities/City.cs
+++ Entities/City.cs
@@ -17,7 +17,7 @@ public class City
     [MaxLength(200)]
     public string? Description { get; set; }
 
-    public ICollection<PointOfInterestDto> PointsOfInterest { get; set; } = new List<PointOfInterestDto>();
+    public ICollection<PointOfInterest> PointsOfInterest { get; set; } = new List<PointOfInterest>();
 
     public City(string name)
     {
