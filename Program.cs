b23371f33517b93fe78553f10f81797335fac1f6
Jaimin Parmar <jaimins100@gmail.com>
2024-04-07 12:02:32 +0530

    create data repository

--- Program.cs
+++ Program.cs
@@ -41,6 +41,8 @@ builder.Services.AddSingleton<CitiesDataStore>();
 builder.Services.AddDbContext<CityInfoContext>(dbContextOptions =>
     dbContextOptions.UseSqlite(builder.Configuration["ConnectionStrings:CityInfoDBConnectionString"]));
 
+builder.Services.AddScoped<ICityInfoRepository, CityInfoRepository>();
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
