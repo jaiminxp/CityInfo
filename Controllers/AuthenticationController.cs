70165409ae9f235bf6c42e85534539ff68c07c68
Jaimin Parmar <jaimins100@gmail.com>
2024-04-12 08:22:41 +0530

    create token

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,85 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CityInfo.API.Controllers;
+
+[Route("api/authentication")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    public class AuthenticationRequestBody
+    {
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+    }
+
+    private class CityInfoUser
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+        public CityInfoUser(int userId, string userName, string firstName, string lastName, string city)
+        {
+            UserId = userId;
+            UserName = userName;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+    }
+
+    private IConfiguration _configuration;
+
+    public AuthenticationController(IConfiguration configuration)
+    {
+        _configuration = configuration ?? throw new ArgumentException(nameof(configuration));
+    }
+
+    [HttpPost("authenticate")]
+    public ActionResult<string> Authenticate(AuthenticationRequestBody authenticationRequestBody)
+    {
+        var user = ValidUserCredentials(
+            authenticationRequestBody.UserName,
+            authenticationRequestBody.Password);
+
+        if (user == null)
+        {
+            return Unauthorized();
+        }
+
+        var securityKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration["Authentication:SecretForKey"]));
+        var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var claimsForToken = new List<Claim>();
+        claimsForToken.Add(new Claim("sub", user.UserId.ToString()));
+        claimsForToken.Add(new Claim("given_name", user.FirstName));
+        claimsForToken.Add(new Claim("family_name", user.LastName));
+        claimsForToken.Add(new Claim("city", user.City));
+
+        var jwtSecurityToken = new JwtSecurityToken(
+            _configuration["Authentication:Issuer"],
+            _configuration["Authentication:Audience"],
+            claimsForToken,
+            DateTime.UtcNow,
+            DateTime.UtcNow.AddHours(1),
+            signingCredentials);
+
+        var tokenToReturn = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+
+        return Ok(tokenToReturn);
+    }
+
+    private CityInfoUser ValidUserCredentials(string? userName, string? password)
+    {
+        // assume that the credentials are valid/
+        return new CityInfoUser(1, userName ?? "", "Jaimin", "Parmar", "Ahmedabad");
+    }
+
+
+}
